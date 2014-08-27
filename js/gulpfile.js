362cbdb3ffb59b3ea8464f3266f69d1369d36576
donglei <xiaosan@outlook.com>
2014-08-27 16:58:30 +0800

    jade

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,15 +1,17 @@
-// npm install gulp gulp-coffee gulp-concat gulp-uglify gulp-imagemin gulp-sourcemaps del
+// npm install gulp gulp-coffee gulp-concat gulp-uglify gulp-imagemin gulp-sourcemaps del gulp-jade
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
+var sourcemaps = require('gulp-jade');
 var del = require('del');
 
 var paths = {
   scripts: ['coffee/**/*.coffee', '!/external/**/*.coffee', 'coffee/*.coffee'],
-  images: 'img/**/*'
+  images: 'img/**/*',
+  jades:['jade/*.jade']
 };
 
 // Not all tasks need to use streams
@@ -24,7 +26,7 @@ gulp.task('scripts', ['clean'], function() {
   // with sourcemaps all the way down
   return gulp.src(paths.scripts)
     .pipe(sourcemaps.init())
-      .pipe(coffee())
+      .pipe(coffee({bare: true}))
       .pipe(uglify())
       .pipe(concat('all.min.js'))
     .pipe(sourcemaps.write())
@@ -39,12 +41,21 @@ gulp.task('images', ['clean'], function() {
     .pipe(gulp.dest('build/img'));
 });
 
+gulp.task('jade', function(){
+	return gulp.src(paths.jades)
+		.pipe(jade({
+		  locals: 'build/jade'
+		}))
+		.pipe(gulp.dest('build/jade'));
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.jades, ['jade']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts', 'images']);
+gulp.task('default', ['watch', 'scripts', 'images', 'jade']);
 
